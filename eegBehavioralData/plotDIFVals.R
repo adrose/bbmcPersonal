21b9bfdedd41e6f0b15f2963f310b068fc67842b
Adon <adon.rosen@gmail.com>
2021-06-03 16:56:59 -0500

    Add script which will be used to plot ICC for items with DIF

---
+++ eegBehavioralData/plotDIFVals.R
@@ -0,0 +1,383 @@
+### This script will be used to plot the DIF effects when the interaction of AMP & LAT are significant
+### This script will not run the IRT models; but will take the output from MPlus and plot the ICC in ggplot2
+### The models will be writeen on the PC and transfered over to the linux workstation
+
+
+# --------------------------------------------------------
+## The model results for CRYING ITEM 15::::
+# F          ON
+# P2AMP              0.150      0.147      1.020      0.308
+# P2LAT              0.076      0.141      0.537      0.591
+# P2INT             -0.060      0.162     -0.372      0.710
+# 
+# V15        ON
+# P2AMP              0.207      0.217      0.955      0.340
+# P2LAT              0.215      0.225      0.958      0.338
+# P2INT              0.520      0.245      2.125      0.034
+# 
+# Thresholds
+# V1$1              -0.853      0.210     -4.062      0.000
+# V2$1              -0.344      0.205     -1.675      0.094
+# V3$1              -1.990      0.385     -5.169      0.000
+# V4$1              -0.840      0.223     -3.767      0.000
+# V5$1              -1.764      0.323     -5.464      0.000
+# V6$1              -1.477      0.297     -4.970      0.000
+# V7$1              -2.549      0.477     -5.344      0.000
+# V8$1              -0.076      0.188     -0.404      0.686
+# V9$1              -1.950      0.354     -5.507      0.000
+# V10$1             -1.551      0.339     -4.575      0.000
+# V11$1             -2.135      0.364     -5.868      0.000
+# V12$1             -2.964      0.516     -5.744      0.000
+# V13$1             -2.249      0.355     -6.343      0.000
+# V14$1             -0.569      0.198     -2.872      0.004
+# V15$1             -2.408      0.425     -5.667      0.000
+# V16$1             -1.469      0.297     -4.939      0.000
+# V17$1             -1.926      0.314     -6.129      0.000
+# V18$1             -2.108      0.332     -6.357      0.000
+# V19$1             -1.108      0.226     -4.900      0.000
+# V20$1             -1.649      0.307     -5.376      0.000
+# V21$1             -1.000      0.270     -3.706      0.000
+# V22$1             -2.133      0.371     -5.744      0.000
+# V23$1             -1.227      0.368     -3.339      0.001
+# V24$1              0.292      0.177      1.650      0.099
+# --------------------------------------------------------
+## The discrimination for question 15 is: 1.399
+
+
+# --------------------------------------------------------
+## The model results for CRYING ITEM 11::::
+# 
+# F          ON
+# P2AMP              0.160      0.151      1.055      0.291
+# P2LAT              0.069      0.142      0.483      0.629
+# P2INT             -0.059      0.165     -0.358      0.721
+# 
+# V11        ON
+# P2AMP             -0.079      0.294     -0.267      0.789
+# P2LAT              0.433      0.284      1.525      0.127
+# P2INT              0.466      0.224      2.080      0.038
+# 
+# Thresholds
+# V1$1              -0.853      0.210     -4.065      0.000
+# V2$1              -0.344      0.205     -1.681      0.093
+# V3$1              -1.989      0.384     -5.175      0.000
+# V4$1              -0.841      0.222     -3.780      0.000
+# V5$1              -1.764      0.322     -5.470      0.000
+# V6$1              -1.480      0.298     -4.962      0.000
+# V7$1              -2.546      0.473     -5.381      0.000
+# V8$1              -0.076      0.188     -0.404      0.686
+# V9$1              -1.949      0.352     -5.537      0.000
+# V10$1             -1.550      0.338     -4.579      0.000
+# V11$1             -2.501      0.412     -6.075      0.000
+# V12$1             -2.967      0.515     -5.757      0.000
+# V13$1             -2.254      0.354     -6.364      0.000
+# V14$1             -0.570      0.199     -2.870      0.004
+# V15$1             -2.131      0.346     -6.156      0.000
+# V16$1             -1.467      0.297     -4.945      0.000
+# V17$1             -1.923      0.313     -6.145      0.000
+# V18$1             -2.100      0.327     -6.420      0.000
+# V19$1             -1.106      0.225     -4.906      0.000
+# V20$1             -1.650      0.307     -5.378      0.000
+# V21$1             -1.000      0.269     -3.719      0.000
+# V22$1             -2.134      0.371     -5.750      0.000
+# V23$1             -1.229      0.367     -3.350      0.001
+# V24$1              0.291      0.177      1.649      0.099
+
+## The model discrimination is: 1.512
+
+# --------------------------------------------------------
+## The model results for UNHAPPY ITEM 85::::
+# F          ON
+# P2AMP             -0.077      0.135     -0.569      0.569
+# P2LAT             -0.091      0.116     -0.779      0.436
+# P2INT             -0.175      0.121     -1.448      0.148
+# 
+# V85        ON
+# P2AMP             -0.018      0.169     -0.106      0.916
+# P2LAT             -0.169      0.189     -0.895      0.371
+# P2INT             -0.489      0.231     -2.114      0.035
+# 
+# Thresholds
+# V73$1              0.592      0.354      1.670      0.095
+# V74$1              1.714      0.208      8.259      0.000
+# V75$1              0.877      0.359      2.441      0.015
+# V76$1             -0.161      0.266     -0.606      0.545
+# V77$1              0.304      0.336      0.905      0.365
+# V78$1              0.098      0.226      0.433      0.665
+# V79$1              0.834      0.321      2.597      0.009
+# V80$1              0.477      0.218      2.186      0.029
+# V81$1              0.826      0.243      3.405      0.001
+# V82$1             -0.338      0.299     -1.130      0.258
+# V83$1              1.347      0.221      6.097      0.000
+# V84$1              0.343      0.294      1.165      0.244
+# V85$1              0.008      0.304      0.025      0.980
+# V86$1              1.060      0.240      4.410      0.000
+# V87$1              0.992      0.224      4.422      0.000
+# V88$1              0.166      0.267      0.622      0.534
+# V89$1              0.692      0.228      3.038      0.002
+# V90$1             -0.549      0.323     -1.699      0.089
+# V91$1              0.748      0.213      3.509      0.000
+# V92$1              1.678      0.254      6.602      0.000
+# V93$1              0.268      0.274      0.977      0.328
+# V94$1             -0.010      0.225     -0.043      0.966
+# V95$1              0.262      0.355      0.739      0.460
+# V96$1              1.006      0.244      4.119      0.000
+
+## The model discrimination is: 1.587
+
+# --------------------------------------------------------
+## The model results for NEUTRAL ITEM 53::::
+# F          ON
+# P2AMP              0.127      0.132      0.959      0.337
+# P2LAT             -0.157      0.144     -1.092      0.275
+# P2INT              0.049      0.194      0.255      0.799
+# 
+# V53        ON
+# P2AMP             -0.160      0.209     -0.769      0.442
+# P2LAT             -0.242      0.153     -1.587      0.112
+# P2INT             -0.359      0.181     -1.984      0.047
+# 
+# Thresholds
+# V49$1             -1.054      0.229     -4.601      0.000
+# V50$1             -1.683      0.305     -5.526      0.000
+# V51$1             -1.210      0.300     -4.034      0.000
+# V52$1             -1.575      0.371     -4.251      0.000
+# V53$1             -0.890      0.237     -3.752      0.000
+# V54$1             -0.539      0.215     -2.510      0.012
+# V55$1             -1.704      0.327     -5.219      0.000
+# V56$1              0.068      0.220      0.312      0.755
+# V57$1             -0.886      0.281     -3.158      0.002
+# V58$1             -1.387      0.310     -4.470      0.000
+# V59$1             -0.941      0.293     -3.215      0.001
+# V60$1             -0.882      0.242     -3.642      0.000
+# V61$1             -1.560      0.342     -4.566      0.000
+# V62$1             -0.404      0.362     -1.117      0.264
+# V63$1             -1.304      0.371     -3.512      0.000
+# V64$1             -1.105      0.299     -3.701      0.000
+# V65$1              1.076      0.272      3.961      0.000
+# V66$1             -0.837      0.287     -2.918      0.004
+# V67$1             -1.522      0.359     -4.239      0.000
+# V68$1             -0.454      0.217     -2.095      0.036
+# V69$1             -0.814      0.243     -3.345      0.001
+# V70$1             -1.065      0.323     -3.299      0.001
+# V71$1             -1.091      0.263     -4.140      0.000
+# V72$1             -1.646      0.297     -5.535      0.000
+
+# The discrimination is: 1.293
+
+# --------------------------------------------------------
+## The model results for NEUTRAL ITEM 62::::
+
+# F          ON
+# P2AMP              0.115      0.131      0.879      0.379
+# P2LAT             -0.163      0.146     -1.113      0.266
+# P2INT              0.001      0.197      0.003      0.998
+# 
+# V62        ON
+# P2AMP              0.113      0.220      0.516      0.606
+# P2LAT             -0.081      0.296     -0.273      0.784
+# P2INT              0.791      0.318      2.485      0.013
+# 
+# Thresholds
+# V49$1             -1.042      0.231     -4.514      0.000
+# V50$1             -1.671      0.305     -5.473      0.000
+# V51$1             -1.182      0.297     -3.979      0.000
+# V52$1             -1.566      0.371     -4.217      0.000
+# V53$1             -0.980      0.242     -4.050      0.000
+# V54$1             -0.530      0.215     -2.469      0.014
+# V55$1             -1.685      0.324     -5.193      0.000
+# V56$1              0.083      0.220      0.379      0.705
+# V57$1             -0.859      0.279     -3.082      0.002
+# V58$1             -1.379      0.313     -4.409      0.000
+# V59$1             -0.930      0.296     -3.147      0.002
+# V60$1             -0.864      0.243     -3.556      0.000
+# V61$1             -1.534      0.337     -4.553      0.000
+# V62$1             -0.617      0.382     -1.616      0.106
+# V63$1             -1.261      0.361     -3.496      0.000
+# V64$1             -1.076      0.294     -3.658      0.000
+# V65$1              1.088      0.272      4.006      0.000
+# V66$1             -0.819      0.287     -2.851      0.004
+# V67$1             -1.505      0.360     -4.185      0.000
+# V68$1             -0.444      0.216     -2.054      0.040
+# V69$1             -0.797      0.241     -3.309      0.001
+# V70$1             -1.038      0.320     -3.241      0.001
+# V71$1             -1.076      0.261     -4.128      0.000
+# V72$1             -1.632      0.297     -5.501      0.000
+
+#  The discrimination is: 1.952
+
+# load-library--------------------------------------------------------
+library("tidyverse")
+library("GGally")
+library("reshape2")
+
+## Load data
+## Now load the latest two stage data from Emma
+amp.vals <- read.csv("./data/eegData/ampVals.csv")
+lat.vals <- read.csv("./data/eegData/latVals.csv")
+## NOw add these back into the eeg Pred values
+amp.vals <- tidyr::pivot_wider(amp.vals, id_cols="ERPset", names_from="binlabel", values_from="value") %>% 
+  mutate(record_id = as.character(ERPset)) %>% 
+  mutate(record_id = if_else(record_id %in% "117-115", "118-64", record_id)) %>%
+  melt(.) %>% 
+  mutate(cycle = substring(sapply(lapply(strsplit(as.character(variable), NULL), rev), paste, collapse=""), 1, 1)) %>%
+  mutate(variable = gsub('.{1}$', '', variable)) %>% 
+  select(-ERPset) %>% 
+  tidyr::pivot_wider(., id_cols=c(record_id, cycle), names_from=c("variable"), values_from=c(value))
+colnames(amp.vals) <- gsub(colnames(amp.vals), pattern = ' ', replacement = "")
+
+lat.vals <- tidyr::pivot_wider(lat.vals, id_cols="ERPset", names_from="binlabel", values_from="value") %>% 
+  mutate(record_id = as.character(ERPset)) %>% 
+  mutate(record_id = if_else(record_id %in% "117-115", "118-64", record_id)) %>%
+  melt(.) %>% 
+  mutate(cycle = substring(sapply(lapply(strsplit(as.character(variable), NULL), rev), paste, collapse=""), 1, 1)) %>%
+  mutate(variable = gsub('.{1}$', '', variable)) %>% 
+  select(-ERPset) %>% 
+  tidyr::pivot_wider(., id_cols=c(record_id, cycle), names_from=c("variable"), values_from=c(value))
+colnames(lat.vals) <- gsub(colnames(amp.vals), pattern = ' ', replacement = "")
+
+## Create the z score values for all of the imaging data
+all.eeg <- merge(amp.vals, lat.vals, by=c("record_id", "cycle"), suffixes = c("_Amp", "_Lat"))
+all.eeg[,3:10] <- scale(all.eeg[,3:10])[,]
+
+## Now prepare the interaction terms
+emo.vals <- c("Crying", "Unhappy", "Neutral", "Happy")
+for(e in emo.vals){
+  ## First declare the column names
+  amp.col <- paste(e, "_Amp", sep='')
+  lat.col <- paste(e, "_Lat", sep='')
+  int.col <- paste(e, "_Int", sep='')
+  # Now estimate the cross prod
+  all.eeg[,int.col] <- all.eeg[,amp.col] * all.eeg[,lat.col]
+}
+
+## Prepare functions
+## Create a function which will operate on unifrom dif items
+returnUnifromICC <- function(baseDIF=NULL, baseDIS=NULL, p2AmpMod=NULL, p2LatMod=NULL, p2IntMod=NULL, p2AmpSubj=NULL, p2LatSubj=NULL, p2IntSubj=NULL){
+  ## First create a range of values for which the ICC will be calulcated over
+  theta.values <- seq(-4, 4, by=.11)
+  ### Now create the formula which will be used to return proabability of endorsment across the entire range of theta values
+  all.vals <- -(baseDIF + p2LatMod * p2LatSubj + p2AmpMod * p2AmpMod + p2IntMod * p2IntSubj) + baseDIS * theta.values / 1 + -(baseDIF + p2LatMod * p2LatSubj + p2AmpMod * p2AmpMod + p2IntMod * p2IntSubj) + baseDIS * theta.values
+  all.vals <- exp(all.vals) / (1 + exp(all.vals))
+  all.vals <-  cbind(theta.values, all.vals)
+  all.vals <- data.frame(all.vals)
+  colnames(all.vals) <- c("theta", "probEndorse")
+  ### Now return these values
+  return(all.vals)
+}
+
+## Now create f unction which will perform the same steps but for nonunifrom DIF
+returnNunifromICC <- function(baseDIF=NULL, baseDIS=NULL, p2AmpMod=NULL, p2LatMod=NULL, p2IntMod=NULL, p2AmpSubj=NULL, p2LatSubj=NULL, p2IntSubj=NULL, p2Ampn=NULL, p2Latn=NULL, p2Intn=NULL){
+  ## First create a range of values for which the ICC will be calulcated over
+  theta.values <- seq(-6, 6, by=.11)
+  ### Now create the formula which will be used to return proabability of endorsment across the entire range of theta values
+  all.vals <- -(baseDIF + p2LatMod * p2LatSubj + p2AmpMod * p2AmpMod + p2IntMod * p2IntSubj) + (baseDIS + p2Ampn*p2AmpSubj + p2Intn*p2Intn + p2Intn*p2IntSubj ) * theta.values / 1 + -(baseDIF + p2LatMod * p2LatSubj + p2AmpMod * p2AmpMod + p2IntMod * p2IntSubj) + (baseDIS + p2Ampn*p2AmpSubj + p2Intn*p2Intn + p2Intn*p2IntSubj ) * theta.values
+  all.vals <- exp(all.vals) / (1 + exp(all.vals))
+  all.vals <-  cbind(theta.values, all.vals)
+  all.vals <- data.frame(all.vals)
+  colnames(all.vals) <- c("theta", "probEndorse")
+  ### Now return these values
+  return(all.vals)
+}
+
+## Now in a loop prepare all of the coefficient values
+emotion.value <- c("Crying", "Crying", "Unhappy", "Neutral", "Neutral", "Neutral", "Happy", "Happy", "Happy")
+base.dif.vals <- c(-2.408, -2.501, 0.012, -0.890, -0.612, -1.503, -2.588, -2.789, -0.588)
+base.dis.vals <- c(1.399, 1.512, 1.587, 1.293, 1.952, 1.694, 1.623, 2.528, 0.977)
+p2AmpModvals <- c(0.207, -0.079, -0.018, -0.160, 0.113, 0.248, -0.103, -0.086, -0.253)
+p2LatModvals <- c(0.215, 0.433, -0.169, -0.242, -0.081, 0.053, -0.123, -0.270, -0.284)
+p2IntModvals <- c(0.520, 0.466, -0.489, -0.359, 0.791, 0.610, .386, -0.643, -0.342)
+all.plot.vals <- NULL
+## Now loop through all of the base values
+for(i in 1:length(base.dif.vals)){
+  base.dif <- base.dif.vals[i]
+  base.dis <- base.dis.vals[i]
+  p2.amp <- p2AmpModvals[i]
+  p2.lat <- p2LatModvals[i]
+  p2.int <- p2IntModvals[i]
+  e <- emotion.value[i]
+  ## Now declare the column names so we can isolate the subject values
+  amp.col <- paste(e, "_Amp", sep='')
+  lat.col <- paste(e, "_Lat", sep='')
+  int.col <- paste(e, "_Int", sep='')
+  all.subj.vals <- unique(all.eeg[,c(amp.col, lat.col, int.col)])
+  ## Now loop through all of the subject values and estimate the ICC for each eeg pattern
+  for(s in 1:dim(all.subj.vals)[1]){
+    tmp.vals <- returnUnifromICC(baseDIF = base.dif, baseDIS = base.dis, p2AmpMod = p2.amp, p2LatMod = p2.lat, p2IntMod = p2.int, p2AmpSubj = all.subj.vals[s,1], p2LatSubj = all.subj.vals[s,2], p2IntSubj = all.subj.vals[s,3])
+    # Now prepare these for the output dataframe
+    subj.amp <- all.subj.vals[s,1]
+    subj.lat <- all.subj.vals[s,2]
+    subj.int <- all.subj.vals[s,3]
+    out.vals <- cbind(i, base.dif, base.dis, p2.amp, p2.lat, p2.int, e, s, subj.amp, subj.lat, subj.int, tmp.vals)
+    all.plot.vals <- rbind(all.plot.vals, out.vals)
+  }
+}
+
+## Now clean these data
+all.plot.vals$subjMagnitude <- abs(all.plot.vals$subj.int)
+
+## Now plot these
+all.plot.vals %>% ggplot(., aes(x=theta, y=probEndorse, color=subjMagnitude, group=s)) +
+  geom_line() +
+  theme_bw() +
+  facet_wrap(i~ e ) +
+  ggtitle("Unifrom DIF ICCs") +
+  ylab("Probability of Correct Response") +
+  xlab("Theta")
+
+
+##############################################################
+#### Work with nonunifrom DIF here
+##############################################################
+## Now in a loop prepare all of the coefficient values
+emotion.value <- c("Crying", "Crying", "Crying", "Neutral", "Neutral", "Neutral", "Happy")
+base.dif.vals <- c(-2.025, -1.529, -2.258, -0.905, -1.009, -0.911, -2.329)
+base.dis.vals <- c(1.318, 1.476, 1.107, 1.569, 1.534, 0.768, 1.393)
+p2AmpModvals <-  c(0.225, 0.424, 0.060, 0.193, -0.372, -0.136, 0.014)
+p2LatModvals <-  c(0.613, -0.540, 0.355, -0.138, 0.386, 0.008, -0.064)
+p2IntModvals <-  c(-0.526, -0.841, 0.087, -0.132, -0.758, -0.043, -0.263)
+p2AmpModDvals <- c(-0.059, 0.508, -0.338, -0.043, -0.741, -0.580, -0.480)
+p2LatModDvals <- c(0.034, -0.693, 0.233, -0.129, -0.153, 0.007, 0.389)
+p2IntModDvals <- c(-1.239, -1.127, -0.873, -0.801, -0.809, -0.603, 0.622)
+all.plot.vals <- NULL
+## Now loop through all of the base values
+for(i in 1:length(base.dif.vals)){
+  base.dif <- base.dif.vals[i]
+  base.dis <- base.dis.vals[i]
+  p2.amp <- p2AmpModvals[i]
+  p2.lat <- p2LatModvals[i]
+  p2.int <- p2IntModvals[i]
+  p2.ampn <- p2AmpModDvals[i]
+  p2.latn <- p2LatModDvals[i]
+  p2.intn <- p2IntModDvals[i]
+  e <- emotion.value[i]
+  ## Now declare the column names so we can isolate the subject values
+  amp.col <- paste(e, "_Amp", sep='')
+  lat.col <- paste(e, "_Lat", sep='')
+  int.col <- paste(e, "_Int", sep='')
+  all.subj.vals <- unique(all.eeg[,c(amp.col, lat.col, int.col)])
+  ## Now loop through all of the subject values and estimate the ICC for each eeg pattern
+  for(s in 1:dim(all.subj.vals)[1]){
+    tmp.vals <- returnNunifromICC(baseDIF = base.dif, baseDIS = base.dis, p2AmpMod = p2.amp, p2LatMod = p2.lat, 
+                                  p2IntMod = p2.int, p2AmpSubj = all.subj.vals[s,1], p2LatSubj = all.subj.vals[s,2], p2IntSubj = all.subj.vals[s,3],
+                                  p2Ampn = p2.ampn, p2Latn = p2.latn, p2Intn = p2.intn)
+    # Now prepare these for the output dataframe
+    subj.amp <- all.subj.vals[s,1]
+    subj.lat <- all.subj.vals[s,2]
+    subj.int <- all.subj.vals[s,3]
+    out.vals <- cbind(i, base.dif, base.dis, p2.amp, p2.lat, p2.int, e, s, subj.amp, subj.lat, subj.int, tmp.vals)
+    all.plot.vals <- rbind(all.plot.vals, out.vals)
+  }
+}
+## Now clean these data
+all.plot.vals$subjMagnitude <- abs(all.plot.vals$subj.int)
+
+## Now plot these
+all.plot.vals %>% ggplot(., aes(x=theta, y=probEndorse, color=subjMagnitude, group=s)) +
+  geom_line() +
+  theme_bw() +
+  facet_wrap(i ~ e ) +
+  ggtitle("Nonunifrom DIF ICCs") +
+  ylab("Probability of Correct Response") +
+  xlab("Theta")
+
